12404fe1fb091e83f3ee63e0786512d05fb6471a
icastro09 <99233585+icastro09@users.noreply.github.com>
2023-02-17 15:31:20 -0600

    Fix web driver wait (#2)
    * Update on .gitignore
    
    * Use WebDriverWait on ResultPage.java
    
    * Implementation of AjaxElementLocatorFactory and @FindBy

--- src/main/java/pages/ResultPage.java
+++ src/main/java/pages/ResultPage.java
@@ -4,26 +4,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import java.util.List;
 
 public class ResultPage {
     WebDriver driver;
 
-
-    @FindBy(css = "h3")
+    @FindBy(css= "h3")
     public List<WebElement> links;
 
-
-    //Constructor
-    public ResultPage(WebDriver driver){
+    // Constructor
+    public ResultPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(this.driver, this);
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10), this);
     }
 
-    public void clickLink(int position) throws Exception{
-        links.get(position-1).click();
+    public void clickLink(int position) throws Exception {
+        links.get(position - 1).click();
     }
-
 
 }
